f67fd683060a3f1b2f4c60076d8ed681dfcff699
Eduardo Valencia <52044886+MuR4Ki@users.noreply.github.com>
2022-03-10 00:12:53 -0300

    commit inicial

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,36 @@
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent implements OnInit {
+  
+
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  deboucer: Subject<string> = new Subject();
+
+  termino: string = '';
+
+  ngOnInit() {
+    this.deboucer
+      .pipe(debounceTime(300))
+      .subscribe( valor => {
+        this.onDebounce.emit( valor );
+      });
+  }
+
+  buscar(){
+    this.onEnter.emit( this.termino );
+  }
+
+  keyPress() {
+    this.deboucer.next( this.termino );
+  }
+
+}
